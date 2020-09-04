31c67e27de570c5f48acf45c73c682c7fa0ffb74
gorillahobo <galihwicaksono@gmail.com>
2020-09-04 20:59:07 +0700

    minor changes

--- config.h
+++ config.h
@@ -3,12 +3,12 @@
 /* appearance */
 static const unsigned int gappx     = 10;        /* gaps between windows */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static unsigned int borderpx  = 3;        /* border pixel of windows */
-static unsigned int snap      = 20;       /* snap pixel */
-static int showbar            = 1;        /* 0 means no bar */
-static int topbar             = 1;        /* 0 means bottom bar */
+static unsigned int borderpx		= 3;        /* border pixel of windows */
+static unsigned int snap			= 20;       /* snap pixel */
+static int showbar            		= 1;        /* 0 means no bar */
+static int topbar             		= 1;        /* 0 means bottom bar */
 
-static const char *fonts[]          = { "Cascadia Mono:size=9:", "Font Awesome:size=9", "monospace:size=9" };
+static const char *fonts[]          = { "scientifica:size=10","Cascadia Mono:size=9:", "Font Awesome:size=9", "monospace:size=9" };
 static const char dmenufont[]       = "monospace:size=10";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
@@ -34,9 +34,10 @@ static const Rule rules[] = {
 	/* class				instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Vivaldi-stable",		NULL,     NULL,           1<<1,      0,          0,           0,        -1 },
 	{ "St",					NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,					NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
-	{ "ncmpcpp", 			NULL,     NULL,           1<<4,      1,          0,           0,        0 },
-	{ "htop",	 			NULL,     NULL,           0,         1,          0,           0,        0 },
+	{ NULL,					NULL,     "Event Tester", 0,         0,          0,           1,        0 }, /* xev */
+	{ "ncmpcpp", 			NULL,     NULL,           0,         1,          0,           0,        0 },
+	{ "gotop",	 			NULL,     NULL,           0,         1,          0,           0,        0 },
+	{ "alsamixer",	    	NULL,     NULL,           0,         1,          0,           0,        0 },
 };
 
 /* layout(s) */
@@ -108,16 +109,16 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_p,      togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_h,      focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_l,      focusmon,       {.i = +1 } },
+	{ MODKEY,                       XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY|ControlMask,           XK_h,      focusmon,       {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_l,      focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_h,      tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_l,      tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -4 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +4 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
-	{ MODKEY|ControlMask,           XK_h,  	   shiftview,      {.i = -1  } },
-	{ MODKEY|ControlMask,           XK_l,  	   shiftview,      {.i = +1  } },
+	{ MODKEY,                       XK_h,  	   shiftview,      {.i = -1  } },
+	{ MODKEY,                       XK_l,  	   shiftview,      {.i = +1  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
